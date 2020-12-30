72506d1c0510420229cba7a0af5e1c21625c5b00
Kamesuta <kamesuta@gmail.com>
2020-12-30 10:50:37 +0900

    app version for discord

--- app/assets/js/discordwrapper.js
+++ app/assets/js/discordwrapper.js
@@ -15,7 +15,7 @@ exports.initRPC = function(genSettings, servSettings, initialDetails = 'ロー�
         largeImageKey: servSettings.largeImageKey,
         largeImageText: servSettings.largeImageText,
         smallImageKey: genSettings.smallImageKey,
-        smallImageText: genSettings.smallImageText,
+        smallImageText: genSettings.smallImageText.replace(/{v}/g, app.getVersion()),
         startTimestamp: new Date().getTime(),
         instance: false
     }
