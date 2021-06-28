bfb2df894f10eb432ead0f74f4818a8072aa0f1b
ddPn08 <pyn.g.1125@gmail.com>
2021-06-28 18:56:26 +0900

    edit scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,8 @@
 
 git submodule update --init && ./scripts/applyPatches.sh || exit 1
 
-if [ "$1" == "--jar" ]; then
+if [ "$1" == "--build" ]; then
+     cd sasaddLauncher
+     yarn install --ignore-engines
      node build.js
 fi
