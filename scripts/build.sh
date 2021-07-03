a99f23f79da1ef2a6ff6263de7d17ebe8e1b4709
ddPn08 <pyn.g.1125@gmail.com>
2021-07-03 23:34:39 +0900

    edit build setting

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,5 +5,5 @@ git submodule update --init && ./scripts/applyPatches.sh || exit 1
 if [ "$1" == "--build" ]; then
      cd sasaddLauncher
      yarn install --ignore-engines
-     node build.js
+     yarn run dist
 fi
